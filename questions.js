26e7ed8fbcad5db9eecd71edc287d9df4e57392d
plking21 <121332292+plking21@users.noreply.github.com>
2023-10-09 19:19:25 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,178 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+    options: [
+      "Madrid",
+      "Paris",
+      "Rome",
+      "Berlin"
+    ]
+  },
+    {
+    numb: 2,
+    question: "National Bird of india?",
+    answer: "peacock",
+    options: [
+      "Parrot",
+      "peacock",
+      "Ostrich",
+      "Wood pecker"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Where is Silveroak university located?",
+    answer: "Gota",
+    options: [
+      "Vastral",
+      "Maninagar",
+      "Gota",
+      "Bhadaj"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Current prime minister of india?",
+    answer: "Narendra modi",
+    options: [
+      "Narendra modi",
+      "Rahul Gandhi",
+      "Arvind Kejriwal",
+      "Amit Shah"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is the national flower of India?",
+    answer: "Lotus",
+    options: [
+    "Rose",
+    "Lily",
+    "Lotus",
+    "Tulip"
+    ]
+    },
+    
+    {
+      numb: 6,
+      question: "What is symbol of 'PI' in mathematics?",
+      answer: "π",
+      options: [
+      "Σ",
+      "√",
+      "Φ",
+      "π"
+      ]
+      },
+    
+      {
+        numb: 7,
+        question: "What does 'HTML' stand for in the context of web development?",
+        answer: "Hypertext Markup Language",
+        options: [
+        "High Text Markup Language",
+        "Hypertext Transfer Markup Language",
+        "Hypertext Markup Language",
+        "Hyperlink and Text Markup Language"
+        ]
+        },
+    
+    {
+    numb: 8,
+    question: "What is the highest mountain in India?",
+    answer: "Mount Everest",
+    options: [
+    "Mount Everest",
+    "Kangchenjunga",
+    "Nanda Devi",
+    "Annapurna"
+    ]
+    },
+    
+    {
+      numb: 9,
+      question: "What does 'URL' stand for in the context of the internet?",
+      answer: "Uniform Resource Locator",
+      options: [
+      "Universal Record Locator",
+      "Uniform Resource Locator",
+      "User Resource Locator",
+      "Unified Record Locator"
+      ]
+      },
+    
+    {
+      numb: 10,
+      question: "Which symbol is commonly used to represent a 'COMMENT' in many programming languages?",
+      answer: "//",
+      options: [
+      "/*",
+      "//",
+      "##",
+      "--"
+      ]
+      },
+     {
+    numb: 11,
+    question: "Which programming language is commonly used for developing mobile applications for iOS devices?",
+answer: "Swift",
+options: [
+"Java",
+"Python",
+"C#",
+"Swift"
+]
+},
+
+ {
+    numb: 12,
+    question: "What is the primary function of an operating system in a computer?",
+    answer: "Managing hardware and software resources",
+    options: [
+      "Running web browsers",
+      "Playing music",
+      "Managing hardware and software resources",
+      "Printing documents"
+    ]
+  },
+
+ {
+    numb: 13,
+    question: "What is the most widely used programming language in the world?",
+    answer: "JavaScript",
+    options: [
+    "Python",
+    "Java",
+    "C++",
+    "JavaScript"
+    ]
+    },
+
+ {
+    numb: 14,
+    question: "Which data type in C is used to store whole numbers (integers)?",
+    answer: "int",
+    options: [
+      "float",
+      "double",
+      "char",
+      "int"
+    ]
+  },
+
+ {
+    numb: 15,
+    question: "What is the term for a type of programming error that causes a program to stop unexpectedly or crash?",
+    answer: "Bug",
+    options: [
+      "Virus",
+      "Bug",
+      "Trojan",
+      "Malware"
+    ]
+  },
+
+];
